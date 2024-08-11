428c1e306ec56f47f1a8e4519a8043f4d55c326a
trifecta-mbo <matthewb@trifecta-gaming.com>
2024-08-11 18:32:23 +0200

    Create containerd-install.sh

---
+++ containerd-install.sh
@@ -0,0 +1,47 @@
+echo "Make script executable using chmod u+x FILE_NAME.sh"
+
+echo "Containerd installation script"
+echo "Instructions from https://kubernetes.io/docs/setup/production-environment/container-runtimes/"
+
+echo "Creating containerd configuration file with list of necessary modules that need to be loaded with containerd"
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+echo "Load containerd modules"
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+
+echo "Creates configuration file for kubernetes-cri file (changed to k8s.conf)"
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+echo "Applying sysctl params"
+sudo sysctl --system
+
+
+echo "Verify that the br_netfilter, overlay modules are loaded by running the following commands:"
+lsmod | grep br_netfilter
+lsmod | grep overlay
+
+echo "Verify that the net.bridge.bridge-nf-call-iptables, net.bridge.bridge-nf-call-ip6tables, and net.ipv4.ip_forward system variables are set to 1 in your sysctl config by running the following command:"
+sysctl net.bridge.bridge-nf-call-iptables net.bridge.bridge-nf-call-ip6tables net.ipv4.ip_forward
+
+echo "Update packages list"
+sudo apt-get update
+
+echo "Install containerd"
+sudo apt-get -y install containerd
+
+echo "Create a default config file at default location"
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+echo "Restarting containerd"
+sudo systemctl restart containerd
